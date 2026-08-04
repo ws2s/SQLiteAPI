c08f738a7d1d15beeaa51e4b9dec40614ffeb50e
JaWinCode <dev@jawincode.com>
2026-08-04 23:45:18 +0200

    change project target version to .net 10 clean up launchSettings.json and App.razor added newline to Program.cs --> coding convention "should end with a newline" made classes use file scoped namespaces for better readability and less nesting

--- Model/OrderWithStatus.cs
+++ Model/OrderWithStatus.cs
@@ -1,46 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
+﻿namespace BlazingPizza;
 
-namespace BlazingPizza
+public class OrderWithStatus
 {
-    public class OrderWithStatus
+    public readonly static TimeSpan PreparationDuration = TimeSpan.FromSeconds(10);
+    public readonly static TimeSpan DeliveryDuration = TimeSpan.FromMinutes(1); // Unrealistic, but more interesting to watch
+
+    public Order Order { get; set; }
+
+    public string StatusText { get; set; }
+
+    public bool IsDelivered => StatusText == "Delivered";
+
+    public static OrderWithStatus FromOrder(Order order)
     {
-        public readonly static TimeSpan PreparationDuration = TimeSpan.FromSeconds(10);
-        public readonly static TimeSpan DeliveryDuration = TimeSpan.FromMinutes(1); // Unrealistic, but more interesting to watch
+        // To simulate a real backend process, we fake status updates based on the amount
+        // of time since the order was placed
+        string statusText;
+        var dispatchTime = order.CreatedTime.Add(PreparationDuration);
 
-        public Order Order { get; set; }
-
-        public string StatusText { get; set; }
-
-        public bool IsDelivered => StatusText == "Delivered";
-
-        public static OrderWithStatus FromOrder(Order order)
+        if (DateTime.Now < dispatchTime)
         {
-            // To simulate a real backend process, we fake status updates based on the amount
-            // of time since the order was placed
-            string statusText;
-            var dispatchTime = order.CreatedTime.Add(PreparationDuration);
-
-            if (DateTime.Now < dispatchTime)
-            {
-                statusText = "Preparing";
-            }
-            else if (DateTime.Now < dispatchTime + DeliveryDuration)
-            {
-                statusText = "Out for delivery";
-            }
-            else
-            {
-                statusText = "Delivered";
-            }
-
-            return new OrderWithStatus
-            {
-                Order = order,
-                StatusText = statusText
-            };
+            statusText = "Preparing";
+        }
+        else if (DateTime.Now < dispatchTime + DeliveryDuration)
+        {
+            statusText = "Out for delivery";
+        }
+        else
+        {
+            statusText = "Delivered";
         }
 
-
+        return new OrderWithStatus
+        {
+            Order = order,
+            StatusText = statusText
+        };
     }
+
+
 }
