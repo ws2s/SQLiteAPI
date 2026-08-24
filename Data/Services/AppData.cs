4e6b863e11d96e989a871d21769613f98ca29603
ws2s <39894753+ws2s@users.noreply.github.com>
2026-08-24 20:46:59 +0000

    Set appData.BaseUri in App.razor

--- Data/Services/AppData.cs
+++ Data/Services/AppData.cs
@@ -4,7 +4,11 @@ namespace SQLiteAPI.Data.Services;
 public class AppData
 {
     public string CompID { get; set; }
+    public string BaseUri { get; set; }
+    
     public bool PageInitialized { get; set; }
+    public bool isSubmitting { get; set; }
+    public HttpResponseMessage HttpResponse { get; set; }
     
     public List<PizzaSpecial> specials { get; set; } = new List<PizzaSpecial>();
     
