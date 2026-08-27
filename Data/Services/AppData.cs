5f8b3ca6e45ff9236a79c937a020003c30374e1d
ws2s <39894753+ws2s@users.noreply.github.com>
2026-08-27 15:10:32 +0000

    Added MainLayout

--- Data/Services/AppData.cs
+++ Data/Services/AppData.cs
@@ -5,11 +5,5 @@ public class AppData
 {
     public string CompID { get; set; }
     public string BaseUri { get; set; }
-    
-    public bool PageInitialized { get; set; }
-    public bool isSubmitting { get; set; }
-    public HttpResponseMessage HttpResponse { get; set; }
-    
-    public List<PizzaSpecial> specials { get; set; } = new List<PizzaSpecial>();
-    
+        
 }
